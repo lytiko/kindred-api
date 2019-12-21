63842edc52d284737321bdd70f972bcbcfda2302
Sam Ireland <sam.ireland.uk@gmail.com>
2019-12-21 10:50:23 +0000

    Start HTML

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ ROOT_URLCONF = "core.urls"
 INSTALLED_APPS = [
  "django.contrib.contenttypes",
  "django.contrib.staticfiles",
+ "sass_processor",
  "core"
 ]
 
@@ -20,5 +21,15 @@ TIME_ZONE = "UTC"
 MIDDLEWARE = [
  "django.middleware.common.CommonMiddleware",
 ]
+
+TEMPLATES = [{
+ "BACKEND": "django.template.backends.django.DjangoTemplates",
+ "APP_DIRS": True,
+ "OPTIONS": {
+  "context_processors": [
+   "django.template.context_processors.request",
+  ],
+ },
+}]
 
 STATIC_URL = "/static/"
