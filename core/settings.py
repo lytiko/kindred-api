b159208e4e1cb804057c6ed7ea031df398ed7de4
Sam Ireland <sam.ireland.uk@gmail.com>
2020-01-02 21:49:00 +0000

    Add admin panel

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,10 @@ ROOT_URLCONF = "core.urls"
 INSTALLED_APPS = [
  "django.contrib.contenttypes",
  "django.contrib.staticfiles",
+ "django.contrib.auth",
+ "django.contrib.sessions",
+ "django.contrib.messages",
+ "django.contrib.admin",
  "sass_processor",
  "core"
 ]
@@ -19,7 +23,11 @@ USE_TZ = True
 TIME_ZONE = "UTC"
 
 MIDDLEWARE = [
+ "django.contrib.sessions.middleware.SessionMiddleware",
  "django.middleware.common.CommonMiddleware",
+ "django.middleware.csrf.CsrfViewMiddleware",
+ "django.contrib.auth.middleware.AuthenticationMiddleware",
+ "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
 TEMPLATES = [{
@@ -28,6 +36,8 @@ TEMPLATES = [{
  "OPTIONS": {
   "context_processors": [
    "django.template.context_processors.request",
+   "django.contrib.auth.context_processors.auth",
+   "django.contrib.messages.context_processors.messages"
   ],
  },
 }]
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib import admin
 from core.views import *
 
 urlpatterns = [
+ path("admin/", admin.site.urls),
  path("", home, name="home")
 ]
