7b4f79835bb967aebfad375b2ceaf2f0faef4456
Sam Ireland <sam.ireland.uk@gmail.com>
2020-03-03 19:10:50 +0000

    Expand user model properties

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 """Core models needed for kindred functionality."""
 
 import os
+import jwt
+import time
 from django.db import models
-from django.dispatch import receiver
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
@@ -15,3 +17,41 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def create_jwt(self):
+        """Creates a JWT token for the user at the current UTC time.
+        
+        A JWT token consists of three components, separated by a period.
+        
+        The first is the header, which lists the algorithm used to encrypt,
+        base64 encoded.
+        
+        The second is the payload, which contains information such as the name
+        of the person the token represents, and the time the token was created.
+        Again this is a dictionary, base64 encoded.
+        
+        The third segment is a secret which allows the server to verify that it
+        issued this token at some point in the past. The first two sections are
+        encrypted using some algorithm and the server's SECRET_KEY. When the
+        token is encountered again, the server can confirm that encrypting the
+        first two sections does indeed produce the third section."""
+
+        return jwt.encode({
+         "sub": self.id, "name": self.username, "iat": int(time.time())
+        }, settings.SECRET_KEY, algorithm="HS256").decode()
+    
+
+    @property
+    def interactions(self):
+        """All interaction objects associated with this user."""
+
+        from people.models import Interaction
+        return Interaction.objects.filter(person__user=self)
+    
+
+    @property
+    def relationships(self):
+        """All relationship objects associated with this user."""
+
+        from people.models import Relationship
+        return Relationship.objects.filter(person1__user=self, person2__user=self)
--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,7 +1,8 @@
+import time
 from mixer.backend.django import mixer
 from django.test import TestCase
 from core.models import *
-from people.models import Tag
+from people.models import Person, Interaction, Relationship, Tag
 
 class UserTests(TestCase):
 
@@ -10,6 +11,41 @@ class UserTests(TestCase):
         user.set_password("12345678")
         user.save()
         self.assertNotEqual(user.password, "12345678")
+    
+
+    def test_user_tokens(self):
+        user = User.objects.create(email="john@mail.com")
+        token = user.create_jwt()
+        token = jwt.decode(token, settings.SECRET_KEY)
+        self.assertEqual(token["sub"], user.id)
+        self.assertEqual(token["name"], user.username)
+        self.assertLessEqual(time.time() - token["iat"], 2)
+    
+
+    def test_can_get_people(self):
+        user = mixer.blend(User)
+        p1 = mixer.blend(Person, user=user)
+        p2 = mixer.blend(Person, user=user)
+        p3 = mixer.blend(Person)
+        self.assertEqual(list(user.people.all()), [p1, p2])
+    
+
+    def test_can_get_interactions(self):
+        user = mixer.blend(User)
+        i1 = mixer.blend(Interaction, person=mixer.blend(Person, user=user))
+        i2 = mixer.blend(Interaction, person=mixer.blend(Person, user=user))
+        i3 = mixer.blend(Interaction)
+        self.assertEqual(set(user.interactions.all()), {i1, i2})
+    
+
+    def test_can_get_relationships(self):
+        user = mixer.blend(User)
+        person1 = mixer.blend(Person, user=user)
+        person2 = mixer.blend(Person, user=user)
+        r1 = mixer.blend(Relationship, person1=person1, person2=person2)
+        r2 = mixer.blend(Relationship, person1=person1, person2=person2)
+        r3 = mixer.blend(Relationship)
+        self.assertEqual(set(user.relationships.all()), {r1, r2})
     
 
     def test_can_get_tags(self):
