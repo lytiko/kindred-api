9b5d4479aee1e37d8f9bb030a0f97c8b1c4fad1a
Sam Ireland <sam.ireland.uk@gmail.com>
2020-04-01 20:22:20 +0100

    Spin out client

--- core/views.py
+++ core/views.py
@@ -20,17 +20,10 @@ def login(request):
 
     if request.method == "POST":
         data = json.loads(request.body.decode())
-        print(data)
         form = LoginForm(data)
         user = form.validate_credentials()
-        print(user)
         if user:
-            r = JsonResponse({"message": user.create_jwt()})
-            print(r)
-            return r
+            return JsonResponse({"message": user.create_jwt()})
         else:
-            print("Returning", dict(form.errors))
-            r =  JsonResponse({"error": dict(form.errors)}, status=422)
-            print(r)
-            return r
+            return JsonResponse({"error": dict(form.errors)}, status=422)
     return JsonResponse({"error": "Method not allowed"}, status=405)
