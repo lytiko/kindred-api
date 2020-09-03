fc2b02bf18937b1651b135fdc505759d5d830a3f
Sam Ireland <sam.ireland.uk@gmail.com>
2020-09-03 12:09:20 +0100

    Reset

--- core/schema.py
+++ core/schema.py
@@ -6,17 +6,6 @@ class Query(graphene.ObjectType):
     portal through which other attributes are accessed."""
 
     time = graphene.Int()
-    user = graphene.Field("core.queries.UserType")
-
-    def resolve_user(self, info, **kwargs):
-        user = info.context.user
-        if not user: raise GraphQLError('{"user": "Not authorized"}')
-        return info.context.user
 
 
-from core.mutations import SignupMutation, LoginMutation
-class Mutation(graphene.ObjectType):
-    signup = SignupMutation.Field()
-    login = LoginMutation.Field()
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
